7c91d89834378d91ef2992d309555489a0e9879e
belcortes <belcortes@gmail.com>
2018-04-05 16:14:42 -0700

    adding click event to heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,11 +7,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-	hero = {
-    id: 1,
-    name: 'Bel'
-  };
+	selectedHero: Hero;
   heroes = HEROES;
+  
+  onSelect(hero: Hero): void {
+	  this.selectedHero = hero;
+	}
+
   constructor() { }
 
   ngOnInit() {
