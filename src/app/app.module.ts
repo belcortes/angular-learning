7a72d14ab877c75b6d9b1a41d450b9417d353da2
belcortes <belcortes@gmail.com>
2018-04-05 21:22:29 -0700

    adding http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -10,6 +12,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent }  from './hero-search/hero-search.component';
 
 
 @NgModule({
@@ -18,13 +21,21 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule, 
-    FormsModule, AppRoutingModule
+    FormsModule, 
+    AppRoutingModule,
+    HttpClientModule,
+
+    HttpClientInMemoryWebApiModule.forRoot(
+		  InMemoryDataService, { dataEncapsulation: false }
+		)
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
